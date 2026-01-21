2103177fac37edfd72d36e88e201597284609fad
jjjwelch420 <RccsdChatAW@outlook.com>
2026-01-20 20:05:49 -0500

    a

--- external/paths.js
+++ external/paths.js
@@ -1,5 +1,5 @@
 window.PATHS = {
-  css: "https://jjjwelch420.github.io/V2/external/style.css",
+  css: "https://jjjwelch420.github.io/V2/external/styles.css",
   js: "https://jjjwelch420.github.io/V2/external/main.js",
   favicon: "https://jjjwelch420.github.io/V2/external/icon.ico",
   games: "https://jjjwelch420.github.io/V2/external/games.json"
